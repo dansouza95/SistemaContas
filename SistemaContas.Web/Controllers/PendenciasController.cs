8ac081771ead80593b3027fba734d4a8588e3dea
dansouza95 <dansouza-11@hotmail.com>
2014-10-14 18:09:44 -0300

    Update PendenciasController.cs

--- SistemaContas.Web/Controllers/PendenciasController.cs
+++ SistemaContas.Web/Controllers/PendenciasController.cs
@@ -21,7 +21,7 @@ namespace SistemaContas.Web.Controllers
         public IClienteRepository repositoryCliente { get; set; }
 
 
-        [Authorize(Roles = "Usuario, Administrador")]
+        [Authorize(R                oles = "Usuario, Administrador")]
         public ActionResult SemTratativa()
         {
             ViewBag.Lista = repositoryConta.ContasEmAberto(Convert.ToInt32(User.Identity.Name)).OrderBy(x => x.DataVencimento).Where(x => x.StatusConta == "Em aberto").ToList();
